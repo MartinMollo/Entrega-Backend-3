35038acdf4dc255c46910420ced2e3337c82c2ac
MartinMollo <116777040+MartinMollo@users.noreply.github.com>
2024-11-16 17:04:06 -0300

    Add files via upload

---
+++ src/config/database.js
@@ -0,0 +1,26 @@
+
+import mongoose from "mongoose";
+import dotenv from "dotenv-flow";
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  console.error(
+    "Error: MONGO_URI no está definida en las variables de entorno."
+  );
+  process.exit(1);
+}
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB conectado exitosamente");
+  } catch (error) {
+    console.error("Error de conexión a MongoDB:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
